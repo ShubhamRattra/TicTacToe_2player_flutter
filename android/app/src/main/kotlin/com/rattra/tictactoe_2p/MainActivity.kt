fd2118423b82f97d58404f20c2c3a74003a3d9bf
ShubhamRattra <shubhamrattra@gmail.com>
2020-08-01 13:46:12 +0530

    tictactoe for 2 player

---
+++ android/app/src/main/kotlin/com/rattra/tictactoe_2p/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rattra.tictactoe_2p
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(primaryColor: Colors.black),
+      home: new HomePage(),
+    );
+  }
+}
